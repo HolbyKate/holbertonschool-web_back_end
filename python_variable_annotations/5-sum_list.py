0ade952803c26640883a6574502cdb6732988a5c
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-01 13:32:09 -0800

    Fix: add from typing import List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,6 +5,9 @@ of floats as argument and returns their sum as a float
 """
 
 
+from typing import List
+
+
 def sum_list(input_list: List[float]) -> float:
     """Returns sum of list of floats"""
     return sum(input_list)
