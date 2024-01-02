e8774caa99ecddec7c7d35448cdc526079462950
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-02 13:16:38 -0800

    Fix: define delay argument

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,6 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    random_delay: float = random.uniform(0, max_delay)
-    await asyncio.sleep(random_delay)
-    return random_delay
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
