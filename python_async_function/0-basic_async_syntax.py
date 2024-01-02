4c1305e1e4e0cb81ec806274c6cc466b969b22c8
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-02 13:08:18 -0800

    Fix random.delay

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,6 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    random_float = random.uniform(0, max_delay)
-    await asyncio.sleep(random_float)
-    return random_float
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
