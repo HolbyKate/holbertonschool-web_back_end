4af2f8b3cdee1ca0e1c4f529e85f11a192236012
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-02 00:57:10 -0800

    Fix: Tuple[str, float]

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,9 +2,9 @@
 """Complex types - string and int/float to tuple"""
 
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns tuple of string and float/int"""
     return (k, v * v)
