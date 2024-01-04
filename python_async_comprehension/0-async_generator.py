b859ae6bf8e6b19fb26b113112f9e3e44a0acc2a
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-04 06:51:19 -0800

    Fix: add comment and delete int

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,8 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, float, None]:
+async def async_generator() -> Generator[float, None, None]:
+    """The coroutine will loop 10 times, each time asynchronously wait 1 second,"""
     for i in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
