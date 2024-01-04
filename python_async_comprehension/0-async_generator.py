26c92e031eed1cb2f954798a3e42e369e35bb0d1
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-04 06:32:40 -0800

    Coroutine async_generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Write a coroutine called async_generator that takes no argument"""
+
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator [int, float, None]:
+    for i in range(10):
+        yield random.uniform(0, 10)
