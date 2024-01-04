95060bfd7ce22911072822ad21c470526f83562c
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-04 06:41:14 -0800

    Fix: pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator [int, float , None]:
+async def async_generator() -> Generator[int, float, None]:
     for i in range(10):
-        await asyncio.sleep(1)
         yield random.uniform(0, 10)
+        await asyncio.sleep(1)
