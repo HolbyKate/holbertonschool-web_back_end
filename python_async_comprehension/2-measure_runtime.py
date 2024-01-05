cc1a4bce643657877c5ecdeb6ca04a028858eb2d
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-05 12:01:23 -0800

    Fix: changing import 1-async_comprenbsion for less than 10 sec

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,29 +7,11 @@ using asyncio.gather
 
 
 import asyncio
-import random
-from typing import AsyncGenerator, List
 import time
+from typing import List
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """
-    The coroutine will loop 10 times, each time asynchronously wait 1 second
-    """
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
-
-
-async def async_comprehension() -> List[float]:
-    """
-    The coroutine will collect 10 random numbers using
-    an async comprehensing over async_generator
-    """
-    for i in range(10):
-        await asyncio.sleep(1)
-        return [_ async for _ in async_generator()]
-    return []
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
