0ae281801f252323f5b0de912f8e34e59c0866a5
HolbyKAte <cathy.augustin@holbertonstudents.com>
2024-01-09 09:03:39 +0000

    Fix return with .find

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -2,6 +2,9 @@
 """function that lists all documents in a collection"""
 
 
+from pymongo import MongoClient
+
+
 def list_all(mongo_collection):
     """function that lists all documents in a collection"""
-    return mongo_collection()
+    return mongo_collection.find()
