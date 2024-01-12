cc1347f0254292463864402189e86cff8ae875ea
HolbyKAte <cathy.augustin@holbertonstudents.com>
2024-01-12 10:47:00 +0000

    Fix import builtins

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-"""function named index_range that takes two integer arguments page and page_size"""
+"""
+function named index_range that takes two integer arguments
+page and page_size
+"""
+import builtins
 
-import pymongo
 
-
-def index_range(page, page_size):
-    """function named index_range that takes two integer arguments page and page_size"""
+def index_range(page: int, page_size: int):
+    """
+    function named index_range that takes two integer 
+    arguments page and page_size
+    """
     return ((page - 1) * page_size, page * page_size)
