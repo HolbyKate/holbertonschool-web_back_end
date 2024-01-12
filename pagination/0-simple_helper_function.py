d6404f39de5d93dc5066d34731207b101195c7d1
HolbyKAte <cathy.augustin@holbertonstudents.com>
2024-01-12 10:47:57 +0000

    Fix: pycodestyle trailing whitespace

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,7 +8,7 @@ import builtins
 
 def index_range(page: int, page_size: int):
     """
-    function named index_range that takes two integer 
+    function named index_range that takes two integer
     arguments page and page_size
     """
     return ((page - 1) * page_size, page * page_size)
