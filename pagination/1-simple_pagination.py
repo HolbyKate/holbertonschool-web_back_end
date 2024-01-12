77db60a71d4de8806e881ee7e55fce77fd35686e
HolbyKAte <cathy.augustin@holbertonstudents.com>
2024-01-12 14:22:59 +0000

    Fix: changing type by isinstance

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -29,20 +29,15 @@ class Server:
 
         return self.__dataset
 
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         method named get_page that takes two integer arguments page
         with default value 1 and page_size with default value 10.
         """
-        assert type(page, int) and type(page_size, int)
-        assert type(page) > 0 and type(page_size) > 0
-        start, end = index_range(page, page_size)
-        return self.__dataset[start:end]
-    
-        if (start, end) >= len(self.__dataset):
-            return []
-    
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        range = index_range(page, page_size)
+        return self.dataset()[range[0]:range[1]]
 
 def index_range(page: int, page_size: int):
     """
