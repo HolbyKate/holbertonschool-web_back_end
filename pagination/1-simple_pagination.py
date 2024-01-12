5cd81645bb0ff12a8a3cb8ceab3dac11cb385e08
HolbyKAte <cathy.augustin@holbertonstudents.com>
2024-01-12 13:42:40 +0000

    Using get_page and define integer

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Implement a method named get_page that takes two integer arguments page
+with default value 1 and page_size with default value 10
+"""
+
+import builtins
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int):
+    """
+    function named index_range that takes two integer
+    arguments page and page_size
+    """
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            pass
+
+
+def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+    """
+    method named get_page that takes two integer arguments page
+    with default value 1 and page_size with default value 10.
+    """
+    assert type(page) is int and type(page_size) is int
+    assert page > 0 and page_size >0
