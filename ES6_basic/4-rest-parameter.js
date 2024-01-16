3f4a56a0f4d1e13a4b92499e9174f07c108172e6
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-16 11:19:15 +0000

    Fix: return arguments args length

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
-export default function returnHowManyArguments(one = Hello, two = Holberton, three, four = 2020) {
-    return `The number of arguments is ${arguments.length}`;
-}
+export default function returnHowManyArguments(...args) {
+    return args.length;
+  }
