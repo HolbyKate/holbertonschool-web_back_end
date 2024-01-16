e054d008c52356ad8889f0fe58e8cc192248ed9b
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-16 10:37:14 +0000

    Fix: doble =

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
-export default function getSumOfHoods(initialNumber == NONE, expansion1989 == 89, expansion2019 == 19) {
+export default function getSumOfHoods(initialNumber = NONE, expansion1989 = 89, expansion2019 = 19) {
     return initialNumber + expansion1989 + expansion2019;
   }
