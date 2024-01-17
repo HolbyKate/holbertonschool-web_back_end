3c47c0ebe21907ef81c77f9cc5c2009a8221add7
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-17 14:33:32 +0000

    Testing impletation of a class with constructor attributes

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default class HolbertonCourse {
+    constructor() {
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+}
