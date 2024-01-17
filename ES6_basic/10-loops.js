ae42f4531e610bf171c0ef6e9a7fbb2535dadf4c
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-17 09:20:59 +0000

    Fix: newarray

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
-    for (const value of array) {
-        console.log(value);
-        }
-
-    return array;
-  }
+    const newArray = [];
+    for (const idx of array) {
+        newArray.push(appendString + idx);
+    }
+    return newArray;
+    }
