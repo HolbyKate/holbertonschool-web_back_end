b82342d6c7ed3e0da35b0462023452e3e4c1663d
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-17 09:03:49 +0000

    Fix with const and value

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
-    for (let value of array) {
-      value = appendString + value;
+    for (const value of array) {
+      const idx = array
+      array[idx] = appendString + value;
     }
   
     return array;
