9500282b903b319fe3526a3b8a9c5ee668b1a995
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-17 21:26:30 +0000

    Fix: getter and setter

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,32 @@
 /* eslint-disable */
 export default class HolbertonCourse {
-    constructor() {
+    constructor(name, length, students) {
         this._name = name;
         this._length = length;
         this._students = students;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        this._name = newName;
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    set length(newLength) {
+        this._length = newLength;
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set students(newStudents) {
+        this._students = newStudents;
     }
 }
