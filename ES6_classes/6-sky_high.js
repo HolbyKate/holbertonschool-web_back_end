dfa982547441c0aabb478b5cf7a3b6338b24c2d5
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-18 12:49:16 +0000

    Fix: change import without .js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,18 +1,18 @@
 /* eslint-disable */
 
-import Building from '5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
-    
-    get floors() {
-        return this._floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this.floors} floors`;
-    }
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
