df273c71f4a117f901734787379b88d8eb0a0543
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-19 10:32:41 +0000

    Try to fix the code for final output

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -19,10 +19,18 @@ const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  get lastName() {
+    return this._lastName;
   }
 
   get fullName() {
@@ -31,6 +39,10 @@ export class StudentHolberton {
 
   get holbertonClass() {
     return this.holbertonClass;
+  }
+
+  set holbertonClass(newHolbertonClass) {
+    this._holbertonClass = newHolbertonClass;
   }
 
   get fullStudentDescription() {
