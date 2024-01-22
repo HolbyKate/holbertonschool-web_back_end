66fac704f0e355cc14a29f4f7a5b8f02995b0edd
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-01-22 09:09:50 +0000

    Update

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default function createReportObject(employeesList) {
+    const createEmployeesObject = {
+        allEmployees: {
+            ...employeesList,
+        },
+    };
+};
