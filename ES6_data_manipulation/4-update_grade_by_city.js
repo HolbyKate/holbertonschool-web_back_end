c606ccc5852d18ebb5aef97b9c1afd3d31f08609
HolbyKate <6804@holbertonstudents.com>
2024-01-30 07:55:39 +0000

    Try to pass the checker (test)

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.filter((student) => student.location === city).map((student) => {
+    const grade = newGrades.filter((grade) => grade.studentId === student.id)[0];
+    return newGrades
+  });
+}
