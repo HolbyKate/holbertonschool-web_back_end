b160ed66e46104b66b972d759ae5c16375b6985e
HolbyKate <6804@holbertonstudents.com>
2024-05-28 14:14:19 +0000

    Fix indent in def get

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -13,8 +13,8 @@ class BasicCache(BaseCaching):
         if key is not None and item is not None:
             self.cache_data[key] = item
             
-            def get(self, key):
-                """return the value in self.cache_data"""
-                if key is None or key not in self.cache_data:
-                    return None
-                return self.cache_data[key]
+    def get(self, key):
+        """return the value in self.cache_data"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
