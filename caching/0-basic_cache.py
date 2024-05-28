6ef2ad73615a32152274dda7883c36f8eec3e963
HolbyKate <6804@holbertonstudents.com>
2024-05-28 14:20:40 +0000

    Pycodestyle

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -9,12 +9,12 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """create class"""
-    
+
     def put(self, key, item):
         """Add a key and see if it's none or not"""
         if key is not None and item is not None:
             self.cache_data[key] = item
-            
+
     def get(self, key):
         """return the value in self.cache_data"""
         if key is None or key not in self.cache_data:
