73f4d2d184af147a8f58a21fa6b825c91fe62dfc
HolbyKate <6804@holbertonstudents.com>
2024-05-28 14:06:42 +0000

    Create class that inherits from basecaching

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Create a class BasicCache that inherits from BaseCaching and is caching system
+"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+            def get(self, key):
+                if key is None or key not in self.cache_data:
+                    return None
+                return self.cache_data[key]
