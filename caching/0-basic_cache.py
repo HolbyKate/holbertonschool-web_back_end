0a0bbbce103faf1d3894d299d707dd4a27f8dd3c
HolbyKate <6804@holbertonstudents.com>
2024-05-28 14:12:25 +0000

    Add comment in each function

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -8,12 +8,13 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-
+    """Add a key and see if it's none or not"""
     def put(self, key, item):
         if key is not None and item is not None:
             self.cache_data[key] = item
-
+            
             def get(self, key):
+                """return the value in self.cache_data"""
                 if key is None or key not in self.cache_data:
                     return None
                 return self.cache_data[key]
