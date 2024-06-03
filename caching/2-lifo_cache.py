3cf45f69aaf10a77cdb955b3ae432376cb1546a7
HolbyKate <6804@holbertonstudents.com>
2024-06-03 15:26:19 +0000

    Create a class LIFOCache that inherits from BaseCaching and is a caching system

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Create a class LIFOCache that inherits from BaseCaching and is caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Create class"""
+    def __init__(self):
+        super().__init__()
+        self.cache_keys = []
+
+    def put(self, key, item):
+        """Assign the item value for key cache"""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_keys.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard_key = self.cache_order.pop(0)
+            print(f"DISCARD: {discard_key}")
+            del self.cache_data[discard_key]
+
+    def get(self, key):
+        """Return the value giving with key in the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
+        
