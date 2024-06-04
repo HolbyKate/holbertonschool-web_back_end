0a83a404bd2cc58fb90f106c329b6725d47dd27b
HolbyKate <6804@holbertonstudents.com>
2024-06-04 08:18:04 +0000

    Same code that LRU but discard key when the cache is full

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Create a class MRUCache that inherits from BaseCaching and is caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Create class"""
+    def __init__(self):
+        super().__init__()
+        self.cache_keys = []
+
+    def put(self, key, item):
+        """Assign the item value for key cache"""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_keys.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard_key = self.cache_keys.pop()
+            print(f"DISCARD: {discard_key}")
+            del self.cache_data[discard_key]
+
+        self.cache_keys.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the value giving with key in the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_keys.remove(key)
+        self.cache_keys.append(key)
+        return self.cache_data[key]
