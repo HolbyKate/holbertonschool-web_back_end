4346b02488d89f030ba1257b022e03e947ca03f6
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-18 00:19:23 +0200

    Update class and implement the format method

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Function that returns the log message obfuscated"""
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -11,3 +12,21 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         pattern = rf'({field}=)([^{separator}]*)'
         message = re.sub(pattern, rf'\1{redaction}', message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Update the class to accept a list of str fields constructor arg"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records"""
+        record message()
+        NotImplementedError
