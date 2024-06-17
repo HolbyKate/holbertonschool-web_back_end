cf430dece2929db5645d4dd1141d6018f61c16c9
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-17 23:20:08 +0200

    Fix length of line

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,8 +3,9 @@
 import re
 
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
-    """Obfuscate field message"""
+def filter_datum(fields: list, redaction: str, message: str,
+                 separator: str) -> str:
+    """Obfuscate field message and return log message"""
     for field in fields:
         pattern = rf'({field}=)([^{separator}]*)'
         message = re.sub(pattern, rf'\1{redaction}', message)
