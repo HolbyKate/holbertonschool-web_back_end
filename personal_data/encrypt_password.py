fc5ae785ef2b0bfc434266e3dae83e8a31ccefe4
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-18 23:59:32 +0200

    Fix whitespace around operator

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -2,7 +2,8 @@
 """Encrypting passwords"""
 import bcrypt
 
-def hash_password(password: str) ->bytes:
+
+def hash_password(password: str) -> bytes:
     """function that expects one string argument name password and
     returns a salted, hashed password, which is a byte string."""
     password = b"somepassword"
