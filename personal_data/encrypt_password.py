c90c27a6770866b49319a45727013b431c9e73ab
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-19 00:22:54 +0200

    Addinf def is_valid to valid the password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,5 +6,13 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """function that expects one string argument name password and
     returns a salted, hashed password, which is a byte string."""
-    password = b"somepassword"
-    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """implement is_valid function"""
+    if bcrypt.checkpw(password.encode("utf-8"), hashed_password):
+        return True
+    return False
