4f8bc91da1080a332564ecf156effc20f81e6839
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-21 14:24:06 +0200

    create instance of BasicAuth

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -19,6 +19,9 @@ AUTH_TYPE = getenv('AUTH_TYPE')
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH_TYPE == "auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = (Auth)
 
 
 @app.errorhandler(404)
