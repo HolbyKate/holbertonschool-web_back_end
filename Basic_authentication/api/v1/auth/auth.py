f53a85ead83ff102aee0fde4fd420c6b6e09cfdd
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-21 10:33:49 +0200

    Update method  if the path is not in the list of strings excluded_paths

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -18,7 +18,19 @@ class Auth:
         Returns:
             bool: _description_
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        """assume excluded_paths contains string path always ending by a /"""
+        if not path.endswith('/'):
+            path +='/'
+        """Check if the path is in the excluded_paths"""
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/') and path == excluded_path:
+                return False
+            return True
+
 
     def authorization_header(self, request=None) -> str:
         """
