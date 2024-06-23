6172c704203549068574f6a2e93c99b77599da30
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 22:35:17 +0200

    Update __init__.py with the new

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -5,7 +5,12 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+from Basic_authentication.api.v1 import app
 from api.v1.views.index import *
 from api.v1.views.users import *
 
 User.load_from_file()
+
+from api.v1.views.session_auth import session_auth
+
+app.register_blueprint(session_auth)
