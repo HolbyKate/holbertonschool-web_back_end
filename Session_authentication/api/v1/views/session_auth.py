99f2db2ee98a5fc22742e228ed5c18a8faf6f768
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 22:49:13 +0200

    import only in rigth place: from api.v1.app import auth

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -2,8 +2,7 @@
 """
 View for Session Authentication
 """
-from flask import request, jsonify, abort, make_response
-from api.v1.app import auth
+from flask import request, jsonify
 from api.v1.views import app_views
 from models.user import User
 from os import getenv
@@ -33,9 +32,12 @@ def login():
         if not user.is_valid_password(password):
             return jsonify({"error": "wrong password"}), 401
 
+    from api.v1.app import auth
+
+    user = found_users[0]
     session_id = auth.create_session(user.id)
-    response = make_response(jsonify(user.to_json()))
-    session_name = getenv('SESSION_NAME')
-    response.set_cookie(session_name, session_id)
+    SESSION_NAME = getenv("SESSION_NAME")
+    response = jsonify(user.to_json())
+    response.set_cookie(SESSION_NAME, session_id)
 
     return response
