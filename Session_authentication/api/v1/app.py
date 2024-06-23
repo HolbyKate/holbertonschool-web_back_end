1c923fc5c981070d9a6cb093ed95a6df821aa682
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 18:11:32 +0200

    Update import of sessionauth

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -4,7 +4,6 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth.session_auth import SessionAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -17,12 +16,15 @@ auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
 
-if AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import Auth
-    auth = SessionAuth()
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
