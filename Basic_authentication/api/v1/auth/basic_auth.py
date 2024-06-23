c5d67f6d43cef617231ce884ca58298674a17c5b
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 15:43:38 +0200

    Fix return user_pwd

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -67,7 +67,33 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        if user is_valid_password(user_pwd)
+        if user is_valid_password(user_pwd):
             return user
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Method that overloads Auth & retrieves User instance for request"""
+        auth_header = self.authorization_header(request)
+
+        if not auth_header:
+            return None
+
+        encoded = self.extract_base64_authorization_header(auth_header)
+
+        if not encoded:
+            return None
+
+        decoded = self.decode_base64_authorization_header(encoded)
+
+        if not decoded:
+            return None
+
+        email, pwd = self.extract_user_credentials(decoded)
+
+        if not email or not pwd:
+            return None
+
+        user = self.user_object_from_credentials(email, pwd)
+
+        return user
