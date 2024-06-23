aaddc94d45fe2161fe7b10709777611e8b1661b4
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 18:15:44 +0200

    Modify definition of session class

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,8 +4,9 @@ Auth class for API authentication management
 """
 from flask import request
 from typing import List, TypeVar
+from api.v1.auth.auth import Auth
 
 
-class SessionAuth:
+class SessionAuth(Auth):
     """Session Auth class"""
     pass
