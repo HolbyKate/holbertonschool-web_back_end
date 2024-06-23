152f4ae47f7a0718b24c29c3e510d7beb56a0a76
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 18:07:22 +0200

    Create empty class

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Auth class for API authentication management
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class SessionAuth:
+    """Session Auth class"""
+    pass
