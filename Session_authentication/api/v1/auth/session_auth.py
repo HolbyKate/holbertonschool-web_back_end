b9d9d3f651293a00bf8813866e41d19560261f80
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 18:24:22 +0200

    Fix: import and detsination of user_id

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -8,7 +8,7 @@ import uuid
 
 class SessionAuth(Auth):
     """Session Auth class"""
-    user_id_by_session_id = ()
+    user_id_by_session_id = {}
 
 
 def create_session(self, user_id: str = None) -> str:
@@ -17,5 +17,5 @@ def create_session(self, user_id: str = None) -> str:
         return None
 
     session_id = str(uuid.uuid4())
-    self.user_id_by_session_id(session_id) = user_id
+    self.user_id_by_session_id[session_id] = user_id
     return session_id
