1b6289b2a1f1b44893e27ab607813627fd312f22
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 18:01:29 +0200

    Add session auth instance

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth import SessionAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -16,9 +17,9 @@ auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
 
-if AUTH_TYPE == "auth":
+if AUTH_TYPE == "session_auth":
     from api.v1.auth.auth import Auth
-    auth = Auth()
+    auth = SessionAuth()
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
