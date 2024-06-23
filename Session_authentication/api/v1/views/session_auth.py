2109863f441bae526f43cd364d42f3337197604d
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 22:50:43 +0200

    Not understand what i missing for checker

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
+""" Module of Session authentication views
 """
-View for Session Authentication
-"""
-from flask import request, jsonify
 from api.v1.views import app_views
+from flask import abort, jsonify, request
 from models.user import User
 from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
-    """Handles user login and creates a session"""
-
+    """ POST /auth_session/login
+    Return
+        - Logged in user
+    """
     email = request.form.get('email')
-    password = request.form.get('password')
 
     if not email:
         return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get('password')
+
     if not password:
         return jsonify({"error": "password missing"}), 400
 
@@ -36,7 +39,9 @@ def login():
 
     user = found_users[0]
     session_id = auth.create_session(user.id)
+
     SESSION_NAME = getenv("SESSION_NAME")
+
     response = jsonify(user.to_json())
     response.set_cookie(SESSION_NAME, session_id)
 
