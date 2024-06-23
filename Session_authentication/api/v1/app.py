7f2f0a19a03edc03998d898c3993efe502e57cba
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 18:08:23 +0200

    Update parent file session_auth

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -4,7 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth import SessionAuth
+from api.v1.auth.session_auth import SessionAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -18,7 +18,7 @@ AUTH_TYPE = getenv('AUTH_TYPE')
 
 
 if AUTH_TYPE == "session_auth":
-    from api.v1.auth.auth import Auth
+    from api.v1.auth.session_auth import Auth
     auth = SessionAuth()
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
