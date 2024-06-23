3d26ba7ce5d42709e14fe060455856fbb5727f85
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 22:53:49 +0200

    Last update

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -5,9 +5,9 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from Basic_authentication.api.v1 import app
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.session_auth import *
+
 
 User.load_from_file()
