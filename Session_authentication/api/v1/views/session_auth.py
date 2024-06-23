5ab2591ce10f395a6b3bd0f0451f293137884f83
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 22:39:12 +0200

    Update request  and password

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-"""View for Session Authentication"""
-
-from flask import Blueprint, request, jsonify
+"""
+View for Session Authentication
+"""
+from flask import request, jsonify, abort, make_response
+from api.v1.views import app_views
 from models.user import User
 import os
 
-session_auth = Blueprint('session_auth', __name__)
 
-
-@session_auth.route(
-    '/auth_session/login', methods=['POST'], strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
-    from api.v1.app import auth  # Import here to avoid circular import issues
+    """Handles user login and creates a session"""
+    from api.v1.app import auth  # Import here to avoid circular import
 
     email = request.form.get('email')
     password = request.form.get('password')
@@ -30,10 +30,8 @@ def login():
         return jsonify({"error": "wrong password"}), 401
 
     session_id = auth.create_session(user.id)
-    user_json = user.to_json()
-
-    response = jsonify(user_json)
-    cookie_name = os.getenv('SESSION_NAME')
-    response.set_cookie(cookie_name, session_id)
+    response = make_response(jsonify(user.to_json()))
+    session_name = os.getenv('SESSION_NAME')
+    response.set_cookie(session_name, session_id)
 
     return response
