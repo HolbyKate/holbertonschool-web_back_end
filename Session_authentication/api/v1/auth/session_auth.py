4f6a475ce2a668bfbc64e192b3dd8ec37a47bf32
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-06-23 18:22:00 +0200

    Adding attribute

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -9,4 +9,14 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """Session Auth class"""
-    pass
+    user_id_by_session_id = ()
+
+
+def create_session(self, user_id: str = None) -> str:
+    """Create a session ID for user_id"""
+    if user_id is None or not isinstance(user_id, str):
+        return None
+
+    session_id = str(uuid.uuid4())
+    self.user_id_by_session_id(session_id) = user_id
+    return session_id
