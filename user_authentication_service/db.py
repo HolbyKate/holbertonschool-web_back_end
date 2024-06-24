f4ae93726c0b56a1e1e6c5c275648cb1c94a704f
HolbyKate <6804@holbertonstudents.com>
2024-06-24 15:41:32 +0200

    Change method

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -32,9 +32,6 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Implement add_user method"""
         new_user = User(email=email, hashed_password=hashed_password)
-
         self._session.add(new_user)
-
         self._session.commit()
-
         return new_user
