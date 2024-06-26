7406a49f31d6d857f63f87d0b2476cfbfe0b3723
HolbyKate <6804@holbertonstudents.com>
2024-06-26 22:44:18 +0200

    put  uuid and password before class

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -9,6 +9,20 @@ from sqlalchemy.orm.exc import NoResultFound
 import uuid
 
 
+def _hash_password(self, password: str) -> bytes:
+    """
+    Method that takes in a password string arguments and returns bytes
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+def _generate_uuid(self) -> str:
+    """Generate an unique UUID string"""
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -16,18 +30,6 @@ class Auth:
     def __init__(self):
         """initialization"""
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """
-        Method that takes in a password string arguments and returns bytes
-        """
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed_password
-
-    def _generate_uuid(self) -> str:
-        """Generate an unique UUID string"""
-        return str(uuid.uuid4())
 
     def register_user(self, email: str, password: str) -> User:
         """Registrer user"""
@@ -40,9 +42,9 @@ class Auth:
             """if no email found, we do the registration"""
             hashed_password = self._hash_password(password)
             """Add user to the database and return the User object"""
-            new_user = self._db.add_user(
+            nuser = self._db.add_user(
                 email=email, hashed_password=hashed_password)
-            return new_user
+            return user
 
     def valid_login(self, email: str, password: str) -> bool:
         """
@@ -50,8 +52,8 @@ class Auth:
         and return a boolean
         """
         try:
-            new_user = self._db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(
-                password.encode('utf-8'), new_user.hashed_password)
+                password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
