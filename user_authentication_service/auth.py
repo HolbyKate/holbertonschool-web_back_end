940b51fdf0d99fd2e82539639f3b93520472b9ff
HolbyKate <6804@holbertonstudents.com>
2024-06-26 22:10:03 +0200

    Fix: deleting user in valid_login method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -46,8 +46,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            if user and bcrypt.checkpw(password.encode('utf-8'),
-                                      user.hashed_password):
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
                 return True
         except NoResultFound:
             pass
