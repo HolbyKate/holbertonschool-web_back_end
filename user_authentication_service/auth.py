d8415316f4517a38a93ee5a58f5c4274ecfa9919
HolbyKate <6804@holbertonstudents.com>
2024-06-26 09:59:42 +0200

    Fix: method with bcrypt

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,15 +2,10 @@
 """Hash password"""
 
 import bcrypt
-import hashlib
-import os
 
 
 def _hash_password(password: str) -> bytes:
     """Method that takes in a password string arguments and returns bytes"""
-
-    salt = os.urandom(32)
-    _hashed_password = hashlib.pbkdf2_hmac(
-        'sha256', password.encode('utf-8'), salt, 100000
-        )
-    return salt + _hashed_password
+    salt = bcrypt.gensalt()
+    _hash_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return _hash_password
