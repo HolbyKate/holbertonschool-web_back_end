3dd1aed92980aa014da3636c92752476343960c9
HolbyKate <6804@holbertonstudents.com>
2024-06-26 16:06:00 +0200

    Validation login not expect the right output

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -38,3 +38,17 @@ class Auth:
             user = self._db.add_user(
                 email=email, hashed_password=hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Method taht expect email and password required arguments
+        and return a boolean
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user and bcrypt.hashpw(password.encode('utf-8'),
+                                      user.hashed_password):
+                return True
+        except NoResultFound:
+            pass
+        return False
