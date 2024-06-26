3027ab121e38e86ddc3fcf1416e23b9efafb5bf7
HolbyKate <6804@holbertonstudents.com>
2024-06-26 12:15:22 +0200

    Regster user with post method

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 """Set up Flask"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
+from auth import Auth
 
 app = Flask(__name__)
+app.register.user()
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
-def Welcome():
+def welcome():
     """GET json data"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """Register user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
