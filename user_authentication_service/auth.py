14586df3550db4c3bfa7ffedfd189c10e9050e23
HolbyKate <6804@holbertonstudents.com>
2024-06-26 22:47:38 +0200

    Change method to try

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,54 +2,47 @@
 """Hash password"""
 
 import bcrypt
-
+import uuid
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
-import uuid
-
-
-def _hash_password(self, password: str) -> bytes:
-    """
-    Method that takes in a password string arguments and returns bytes
-    """
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
-
-
-def _generate_uuid(self) -> str:
-    """Generate an unique UUID string"""
-    return str(uuid.uuid4())
 
 
 class Auth:
-    """Auth class to interact with the authentication database.
-    """
+    """Auth class to interact with the authentication database."""
 
     def __init__(self):
-        """initialization"""
+        """Initialization"""
         self._db = DB()
 
+    def _hash_password(self, password: str) -> bytes:
+        """
+        Method that takes in a password string argument and returns bytes.
+        The returned bytes is a salted hash of the input password,
+        hashed with bcrypt.hashpw.
+        """
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
+
+    def _generate_uuid(self) -> str:
+        """Generate a unique UUID string."""
+        return str(uuid.uuid4())
+
     def register_user(self, email: str, password: str) -> User:
-        """Registrer user"""
+        """Register user."""
         try:
-            """Try to find the user by email"""
             self._db.find_user_by(email=email)
-            """if email already existe"""
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            """if no email found, we do the registration"""
             hashed_password = self._hash_password(password)
-            """Add user to the database and return the User object"""
-            nuser = self._db.add_user(
+            user = self._db.add_user(
                 email=email, hashed_password=hashed_password)
             return user
 
     def valid_login(self, email: str, password: str) -> bool:
         """
-        Method taht expect email and password required arguments
-        and return a boolean
+        Method that expects email and password arguments and returns a boolean.
         """
         try:
             user = self._db.find_user_by(email=email)
