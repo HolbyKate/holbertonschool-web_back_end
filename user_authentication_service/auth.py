ebb0ba5f5321872bbae7e10246837dcd10106156
HolbyKate <6804@holbertonstudents.com>
2024-06-26 09:56:17 +0200

    Fix pycodestyle

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -8,10 +8,9 @@ import os
 
 def _hash_password(password: str) -> bytes:
     """Method that takes in a password string arguments and returns bytes"""
-    
+
     salt = os.urandom(32)
-    hashed_password = hashlib.pbkdf2_hmac('sha256', 
-                                          password.encode('utf-8'), 
-                                          salt, 
-                                          100000)
-    return salt + hashed_password
+    _hashed_password = hashlib.pbkdf2_hmac(
+        'sha256', password.encode('utf-8'), salt, 100000
+        )
+    return salt + _hashed_password
