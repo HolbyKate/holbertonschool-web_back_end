c98b7efe9437295f0d9b809ee483d3d57f2af9ff
HolbyKate <6804@holbertonstudents.com>
2024-06-27 10:35:45 +0200

    Fix pycodestyle

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -35,14 +35,14 @@ def login():
     """Create login"""
     email = request.form.get("email")
     password = request.form.get("password")
-    
+
     try:
         AUTH.valid_login(email, password)
     except ValueError:
         return jsonify({"email": "<user email>", "message": "logged in"})
     else:
-        abort(401)  
-        
+        abort(401)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
