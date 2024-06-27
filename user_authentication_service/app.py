15f31a80e4576e88926493bb76348fb74ac43a80
HolbyKate <6804@holbertonstudents.com>
2024-06-27 14:51:50 +0200

    Fix: find user associated with ID

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Set up Flask"""
 
-from flask import Flask, jsonify, request, abort, make_response, sessions
+from flask import Flask, jsonify, request, abort, make_response, redirect
 
 from auth import Auth
 
@@ -44,7 +44,23 @@ def login():
         return response, 200
     else:
         abort(401)
-      
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """Create logoutfunction"""
+    session_id = request.cookies.get('session_id')
+
+    if session_id is None:
+        abort(403)
+    """Find user associate with session_id"""
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    """Destroy session associate with user"""
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
