b6bb3f329226c7b32399f09e6457d42ff153ce26
HolbyKate <6804@holbertonstudents.com>
2024-06-27 23:08:24 +0200

    Reset password token  with email field

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -77,5 +77,17 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """Reset password"""
+    try:
+        email = request.form.get('email')
+        token = AUTH.get_reset_password_token
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
