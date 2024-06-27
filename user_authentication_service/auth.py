f9046ede11e9e8458bd02e09bb20a1ef213a6f24
HolbyKate <6804@holbertonstudents.com>
2024-06-27 11:43:08 +0200

    Destroy session id

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -75,3 +75,12 @@ class Auth:
             return None
         else:
             return User
+
+    def destroy_session(self, user_id: int) -> None:
+        """Shut down session"""
+        try:
+            user_id = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            self._db.update_user(user_id, session_id=None)
