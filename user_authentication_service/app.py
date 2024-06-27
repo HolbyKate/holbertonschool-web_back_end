7ac9af7c325bb2b0709f19c7e5f14f8c7ea1cf0c
HolbyKate <6804@holbertonstudents.com>
2024-06-27 11:21:57 +0200

    fix: email in reponse

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -39,7 +39,7 @@ def login():
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
         response = make_response(jsonify(
-                {"email": "<user email>", "message": "logged in"}))
+                {"email": email, "message": "logged in"}))
         response.set_cookie("session_id", session_id)
         return response
     else:
