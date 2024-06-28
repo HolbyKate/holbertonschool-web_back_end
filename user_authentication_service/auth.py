75efe18f3ca4f3c843f4b2603f5eb8ba3cc6623a
HolbyKate <6804@holbertonstudents.com>
2024-06-28 09:55:48 +0200

    Fix: delete value of value Error

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -91,7 +91,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            raise ValueError((f"User {email} not found"))
+            raise ValueError
         reset_token = _generate_uuid
         self._db.update_user(user.id, reset_token)
         return reset_token
