1bd3f92b0bce1879d06fff4c9380c1548a939a3b
HolbyKate <6804@holbertonstudents.com>
2024-07-03 20:54:54 +0200

    Method that verify exception message

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -17,6 +17,15 @@ class TestAccessNestedMap(unittest.TestCase):
         output = access_nested_map(map, path)
         self.assertEqual(output, expected_output)
 
+    @parameterized.expand([
+        ({}, ("a",), 1),
+        ({"a": 1}, ("a", "b"), 1),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_output):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+            self.assertEqual(str(context.exception), expected_output)
+
 
 if __name__ == "__main__":
     unittest.main()
