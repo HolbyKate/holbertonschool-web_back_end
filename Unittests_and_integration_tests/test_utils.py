2f32e0bbb0a987c46862056dcf49778b8d706e27
HolbyKate <6804@holbertonstudents.com>
2024-07-03 21:37:47 +0200

    Adding testing with mock and tes_playload

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -43,6 +43,8 @@ class TestGetJson(unittest.TestCase):
 
         """call the function with test input"""
         result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
