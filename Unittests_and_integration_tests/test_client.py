4ba2ae6e27a8b37f3652d24553622cd456bf0467
HolbyKate <6804@holbertonstudents.com>
2024-07-04 10:37:35 +0200

    Using PropertyMock that the result of _public_repos_url

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,9 +2,10 @@
 """set up the TestGithubOrgClient"""
 
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
-from client import GithubOrgClient, get_json
+from client import GithubOrgClient
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test cases for GithubOrgClient."""
@@ -23,6 +24,17 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(response, expected_response)
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Method that test expected result"""
+        mock_org.return_value = {"repos_url": "https://api.github.com/orgs/google/repos"}
+
+        client = GithubOrgClient('google')
+        result = client._public_repos_url
+
+        self.assertEqual(result, "https://api.github.com/orgs/google/repos")
+
 
 if __name__ == '__main__':
     unittest.main()
