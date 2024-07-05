6feef31976db4723150a4885dc6dce24b0f88374
HolbyKate <6804@holbertonstudents.com>
2024-07-05 11:02:26 +0200

    Create basic Flask app and index html

---
+++ i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Setup a basic Flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """create a simple route that simply outputs"""
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
