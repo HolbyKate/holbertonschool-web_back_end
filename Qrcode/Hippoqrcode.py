feb661832c2846b4c737857b278bfb4aec430531
HolbyKate <6804@holbertonstudents.com>
2024-07-15 14:22:35 +0200

    Update Qr code for Linkedin link

--- Qrcode/Hippoqrcode.py
+++ Qrcode/Hippoqrcode.py
@@ -4,43 +4,43 @@
 import qrcode
 from PIL import Image
 
-# Créer un QR Code
+# Create a QR code
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=10,
     border=4,
 )
-qr.add_data('https://www.holbertonschool.fr/')
+qr.add_data('https://www.linkedin.com/in/cathyaugustin/')
 qr.make(fit=True)
 
-# Créer une image QR Code
+# Create a QR code image
 img_qr = qr.make_image(fill_color="black", back_color="white").convert('RGB')
 
-# Charger l'image de l'hippocampe
-img_hippocampe = Image.open('Hippo.png')
+# Import hippo imae
+img_hippocampe = Image.open('CM.png')
 
-# Vérifier le mode de l'image et ajuster si nécessaire
+# Check image mode
 if img_hippocampe.mode == 'RGBA':
-    # Utiliser le canal alpha comme masque
-    mask = img_hippocampe.split()[3]  # Le canal alpha est le 4ème canal dans 'RGBA'
+    # Use alpha canal for image
+    mask = img_hippocampe.split()[3]  # The alpha channel is the 4th channel in 'RGBA'.
 else:
-    # Convertir l'image en niveaux de gris pour l'utiliser comme masque
+    # Convert image to grayscale for use as mask
     mask = img_hippocampe.convert('L')
 
-# Redimensionner l'image de l'hippocampe pour qu'elle s'insère dans le QR Code
+# Resize the seahorse image to fit the QR Code
 width, height = img_qr.size
-img_hippocampe = img_hippocampe.resize((int(width * 0.5), int(height * 0.5)))
-mask = mask.resize((int(width * 0.5), int(height * 0.5)))
+img_hippocampe = img_hippocampe.resize((int(width * 0.3), int(height * 0.3)))
+mask = mask.resize((int(width * 0.3), int(height * 0.3)))
 
-# Calculer la position pour centrer l'image de l'hippocampe sur le QR Code
+# Calculate the position to center the image of the hippocampus on the QR Code
 position = ((width - img_hippocampe.width) // 2, (height - img_hippocampe.height) // 2)
 
-# Coller l'hippocampe sur le QR Code en utilisant le masque adapté
+# Paste the seahorse onto the QR Code using the appropriate mask
 img_qr.paste(img_hippocampe, position, mask)
 
-# Sauvegarder l'image finale
-img_qr.save('qrcode_hippocampe.png')
+# Save final image
+img_qr.save('qrcode_hippocampe2.png')
 
-# Afficher l'image
+# Display image
 img_qr.show()
---
+++ Qrcode/README.md
@@ -0,0 +1,5 @@
+Trying to create a QRCODE with hippocampe
+2 install importantes: 
+pip install qrcode[pil]
+pip install Pillow
+et img_qr.show() pour afficher l'image
