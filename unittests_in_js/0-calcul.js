78c27c7805353db7f7304ff20f12d983263e28f5
HolbyKate <6804@holbertonstudents.com>
2024-07-15 14:44:59 +0200

    Fix: update module export

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
 // Create the function calculateNumber that round a and b and return the sum of it
 
-export default function calculateNumber(a, b) {
+function calculateNumber(a, b) {
     return Math.round(a) + Math.round(b);
 }
+
+
+module.exports = calculateNumber;
