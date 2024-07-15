b89da85658ae5cbdbdc03eabbd5091ae7804cc5c
HolbyKate <6804@holbertonstudents.com>
2024-07-15 14:23:45 +0200

    First attempt for Basic test with Mocha and Node assertion library

---
+++ unittests_in_js/0-calcul.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+// Create the function calculateNumber that round a and b and return the sum of it
+
+export default function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
+}
