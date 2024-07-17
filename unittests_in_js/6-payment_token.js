edcbb895e86d84e1c7712cb658f08a15ff7f97b4
HolbyKate <6804@holbertonstudents.com>
2024-07-17 22:48:54 +0200

    Test failed

---
+++ unittests_in_js/6-payment_token.js
@@ -0,0 +1,10 @@
+// Async testing
+
+function getPaymentTokenFromAPI(success) {
+    if (success) {
+        return Promise.resolve({data: 'Successful response from the API' });
+    }
+
+}
+
+module.exports = getPaymentTokenFromAPI;
