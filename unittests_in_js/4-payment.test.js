34b85d850156c7108f812881c9bf56edeb341822
HolbyKate <6804@holbertonstudents.com>
2024-07-17 22:14:08 +0200

    Fix first call on args

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -20,13 +20,12 @@ describe('sendPaymentRequestToApi', function() {
     });
 
     it('should call Utils.calculateNumber with SUM, 100, 20 and log the correct total', function() {
-        const spy = sinon.spy(Utils, 'calculateNumber');
-
         sendPaymentRequestToApi(100, 20);
 
-        expect(spy.calledOnce).to.be.true;
-        expect(spy.calledWith('SUM', 100, 20)).to.be.true;
-
-        spy.restore();
+        expect(calculateNumberStub.calledOnce).to.be.true;
+        expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+        
+        expect(consoleLogSpy.calledOnce).to.be.true;
+        expect(consoleLogSpy.firstCall.args[0]).to.equal('The total is: 10');
     });
 });
