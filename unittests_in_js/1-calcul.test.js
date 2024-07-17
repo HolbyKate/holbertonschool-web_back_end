cafab7fc3cf591f157856dc361e9a5b267b0a874
HolbyKate <6804@holbertonstudents.com>
2024-07-17 11:07:07 +0200

    Adding SUM to the test

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -6,27 +6,27 @@ const calculateNumber = require('./1-calcul.js');
 describe('calculateNumber', () => {
     describe('SUM', () => {
   it('should return 4 when inputs are 1 and 3', function () {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
   });
 
   it('should return 5 when inputs are 1 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber('SUML', 1, 3.7), 5);
   });
 
   it('should return 5 when inputs are 1.2 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
   });
 
   it('should return 6 when inputs are 1.5 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
   });
 
   it('should return 0 when inputs are -0.4 and 0.4', function () {
-    assert.strictEqual(calculateNumber(-0.4, 0.4), 0);
+    assert.strictEqual(calculateNumber('SUM', -0.4, 0.4), 0);
   });
 
   it('should return -1 when inputs are -0.6 and -0.4', function () {
-    assert.strictEqual(calculateNumber(-0.6, -0.4), -1);
+    assert.strictEqual(calculateNumber('SUM', -0.6, -0.4), -1);
   });
 
   it('should return -2 when inputs are -1.4 and -0.6', function () {
@@ -39,5 +39,86 @@ describe('calculateNumber', () => {
 
   it('should return 1 when inputs are -1.5 and 2.4', function () {
     assert.strictEqual(calculateNumber(-1.5, 2.4), 1);
+  });
+});
+
+describe('SUBTRACT', () => {
+    it('should return -2 when inputs are 1 and 3', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
+    });
+
+    it('should return -3 when inputs are 1 and 3.7', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
+    });
+
+    it('should return -3 when inputs are 1.2 and 3.7', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+    });
+
+    it('should return -2 when inputs are 1.5 and 3.7', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
+    });
+
+    it('should return -1 when inputs are -0.4 and 0.4', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', -0.4, 0.4), -1);
+    });
+
+    it('should return -1 when inputs are -0.6 and -0.4', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', -0.6, -0.4), -1);
+    });
+
+    it('should return -1 when inputs are -1.4 and -0.6', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -0.6), -1);
+    });
+
+    it('should return 0 when inputs are -1.5 and -1.4', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', -1.5, -1.4), 0);
+    });
+
+    it('should return -4 when inputs are -1.5 and 2.4', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', -1.5, 2.4), -4);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return 0.25 when inputs are 1 and 3', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 1, 3), 0.25);
+    });
+
+    it('should return 0.25 when inputs are 1 and 3.7', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 1, 3.7), 0.25);
+    });
+
+    it('should return 0.25 when inputs are 1.2 and 3.7', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.2, 3.7), 0.25);
+    });
+
+    it('should return 0.5 when inputs are 1.5 and 3.7', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 0.5);
+    });
+
+    it('should return -1 when inputs are -0.4 and 0.4', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', -0.4, 0.4), -1);
+    });
+
+    it('should return 2 when inputs are -0.6 and -0.4', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', -0.6, -0.4), 2);
+    });
+
+    it('should return 2 when inputs are -1.4 and -0.6', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', -1.4, -0.6), 2);
+    });
+
+    it('should return 1 when inputs are -1.5 and -1.4', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', -1.5, -1.4), 1);
+    });
+
+    it('should return -1 when inputs are -1.5 and 2.4', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', -1.5, 2.4), -1);
+    });
+
+    it('should return Error when inputs are 1.4 and 0.4', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0.4), 'Error');
+    });
   });
 });
