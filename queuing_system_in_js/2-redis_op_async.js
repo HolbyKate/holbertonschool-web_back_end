f57f8361e471ffaded8c2384fa41f9d684a6cb25
HolbyKate <6804@holbertonstudents.com>
2024-07-18 22:58:13 +0200

    Update redis with async operation

--- queuing_system_in_js/2-redis_op_async.js
+++ queuing_system_in_js/2-redis_op_async.js
@@ -1,25 +1,36 @@
-// Node Redis client and basic operations
+// Use the promisify function from the util module to convert 
+// the callback-based client.get method to a promise-based method.
 
 import redis from 'redis';
+import { promisify } from 'util';
+
 const client = redis.createClient();
 
 client.on('connect', () => {
-  console.log('Redis client connected to the server');
+    console.log('Redis client connected to the server');
 });
 
 client.on('error', (err) => {
-  console.log(`Redis client not connected to the server: ${err.message}`);
+    console.log(`Redis client not connected to the server: ${err.message}`);
 });
 
 function setNewSchool(schoolName, value) {
     client.set(schoolName, value, redis.print);
 }
 
-function displaySchoolValue(schoolName) {
-    const finalValue = client.get(schoolName, redis.print);
-    console.log(finalValue);
+// Promisify the client.get function
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+    try {
+        const reply = await getAsync(schoolName);
+        console.log(reply);
+    } catch (err) {
+        console.log(err.message);
+    }
 }
 
+// call the functions as required
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
 displaySchoolValue('HolbertonSanFrancisco');
