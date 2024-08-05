942d3ce776e2660feabcda82b55ecd9345d531c3
HolbyKate <6804@holbertonstudents.com>
2024-08-05 14:15:16 +0200

    Create Cache class with redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache :
+    
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        #Generate a new uuid and store the data in Redis
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
