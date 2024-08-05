25405b0cfecbd8886f5ff39a7acde7eabf9f349d
HolbyKate <6804@holbertonstudents.com>
2024-08-05 14:18:24 +0200

    Adding comment and pycodestyle correction

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,14 +5,19 @@ import uuid
 from typing import Union
 
 
-class Cache :
-    
+class Cache:
+
     def __init__(self):
+        """
+        Initilize Cache class a Redis client instance and flushes the database
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        #Generate a new uuid and store the data in Redis
+        """
+        Generate a new uuid and store the data in Redis
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
