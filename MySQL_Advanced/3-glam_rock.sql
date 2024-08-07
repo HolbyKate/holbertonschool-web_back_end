26f3b78dfd6055e67dbc80f790de591b1cc948d9
HolbyKate <6804@holbertonstudents.com>
2024-08-07 23:46:09 +0200

    Adding if statement

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Lists all bands with Glam rock as their main style, ranked by longevity
-SELECT name AS band_name, IFNULL(YEAR(CURDATE()) - formed, 0) -
-IFNULL(YEAR(CURDATE()) - split, 0) AS lifespan
+SELECT name AS band_name,
+IF(split IS NULL, YEAR(CURDATE()) - formed, split - formed) AS lifespan
 FROM metal_bands
 WHERE style = Glam rock
 ORDER BY lidfespan DESC;
