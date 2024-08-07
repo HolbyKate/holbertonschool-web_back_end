4504c8b0192546d0d5585d48f6b7a915a5f87f36
HolbyKate <6804@holbertonstudents.com>
2024-08-07 11:40:05 +0200

    Restructuration of code

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,7 +7,7 @@ from functools import wraps
 
 
 def call_history(method: Callable) -> Callable:
-    """Decorator to store the history of inputs and outputs"""
+    """Decorator to store the history of inputs and outputs."""
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         key = method.__qualname__
@@ -27,9 +27,7 @@ def call_history(method: Callable) -> Callable:
 
 
 def count_calls(method: Callable) -> Callable:
-    """
-    Decorator to count how many times a method is called.
-    """
+    """Decorator to count how many times a method is called."""
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         key = method.__qualname__
@@ -39,7 +37,6 @@ def count_calls(method: Callable) -> Callable:
 
 
 class Cache:
-
     def __init__(self):
         """
         Initialize Cache class with a Redis client instance and
@@ -51,9 +48,7 @@ class Cache:
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Generate a new uuid and store the data in Redis
-        """
+        """Generate a new uuid and store the data in Redis."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
@@ -73,22 +68,16 @@ class Cache:
         return value
 
     def get_str(self, key: str) -> Union[str, None]:
-        """
-        Retrieve a string value from Redis
-        """
+        """Retrieve a string value from Redis."""
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> Union[int, None]:
-        """
-        Retrieve an integer value from Redis
-        """
+        """Retrieve an integer value from Redis."""
         return self.get(key, fn=int)
 
 
 def replay(method: Callable):
-    """
-    Display the history of calls of a particular method
-    """
+    """Display the history of calls of a particular method."""
     cache_instance = method.__self__
     method_name = method.__qualname__
     input_key = f"{method_name}:inputs"
@@ -101,8 +90,8 @@ def replay(method: Callable):
 
     print(f"{method_name} was called {call_count} times:")
     for inp, outp in zip(inputs, outputs):
-        print(f"{method_name}(*{inp.decode(
-            'utf-8')}) -> {outp.decode('utf-8')}")
+        print(
+            f"{method_name}(*{inp.decode('utf-8')}) -> {outp.decode('utf-8')}")
 
 
 # Testing the implementation
