d7a5d84145feac261dc4363e031c9c2f9e0e99b1
HolbyKate <6804@holbertonstudents.com>
2024-08-09 09:45:49 +0200

    Try other way beacause of checker bug non resolve

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Lists all bands with Glam rock as their main style, ranked by longevity
-SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
-FROM metal_bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT DISTINCT `band_name`, IFNULL(`split`, 2020) - `formed` as `lifespan`
+FROM `metal_bands`
+WHERE FIND_IN_SET('Glam rock', style)
+ORDER BY `lifespan` DESC;
